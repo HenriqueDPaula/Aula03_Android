920d272cbcf10f26c2be0cc93a93cdceb4fa4d5c
opet <henrique.depaula@hotmail.com>
2018-03-14 21:17:09 -0300

    button salvar

--- app/src/main/java/com/example/opet/aula3/MainActivity.java
+++ app/src/main/java/com/example/opet/aula3/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.opet.aula3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
+    private Spinner spinner;
+    private List<String>grupos = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.Spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, grupos);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
 }
