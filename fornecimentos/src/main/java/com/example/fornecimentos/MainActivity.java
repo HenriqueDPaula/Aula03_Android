10b29644335f06f4c060402d27b771ab05552efb
Henrique de Paula <henrique.depaula@hotmail.com>
2018-03-16 13:43:35 -0300

    inicio atividade2

--- fornecimentos/src/main/java/com/example/fornecimentos/MainActivity.java
+++ fornecimentos/src/main/java/com/example/fornecimentos/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.fornecimentos;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -13,9 +17,46 @@ public class MainActivity extends Activity {
     private EditText editVU;
     private EditText editQTD;
     private TextView textResultado;
+    private ListView listView;
+
+    public class Produto {
+        private String nome;
+        private double valorUnitario;
+        private int quantidade;
+
+        public Produto(String nome, double valorUnitario, int quantidade){
+            this.nome = nome;
+            this.valorUnitario = valorUnitario;
+            this.quantidade = quantidade;
+        }
+
+        public double Calcular(){
+
+            double resultado = valorUnitario * quantidade;
+            return resultado;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editProduto = findViewById(R.id.editProduto);
+        editVU = findViewById(R.id.editVU);
+        editQTD = findViewById(R.id.editQTD);
+        listView = findViewById(R.id.lista);
+    }
+
+    public List<Produto>todosOsProdutos(){
+
+        String nome = editProduto.getText().toString();
+        int quantidade = Integer.parseInt(editQTD.getText().toString());
+        double valorUnitario = Double.parseDouble(editVU.getText().toString());
+
+        Produto objeto = new Produto(nome, valorUnitario, quantidade);
+        List<Produto>produtos = new ArrayList<>();
+        produtos.add(objeto);
+
+        return produtos;
     }
 }
